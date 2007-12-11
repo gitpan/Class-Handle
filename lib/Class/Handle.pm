19fce4280b004e72b95d3befd57f37db2a3ba437
Adam Kennedy <adamk@cpan.org>
2007-12-11 04:48:27 +0000

    Import of ADAMK/Class-Handle-1.07 from CPAN.
    gitpan-cpan-distribution: Class-Handle
    gitpan-cpan-version:      1.07
    gitpan-cpan-path:         ADAMK/Class-Handle-1.07.tar.gz
    gitpan-cpan-author:       ADAMK
    gitpan-cpan-maturity:     released

--- lib/Class/Handle.pm
+++ lib/Class/Handle.pm
@@ -82,7 +82,7 @@ use Class::Inspector ();
 # Set the version
 use vars qw{$VERSION};
 BEGIN {
-	$VERSION = '1.06';
+	$VERSION = '1.07';
 }
 
 
@@ -497,7 +497,7 @@ For other inquiries, contact the author
 
 =head1 AUTHOR
 
-Adam Kennedy E<lt>cpan@ali.asE<gt>, L<http://ali.as/>
+Adam Kennedy E<lt>adamk@cpan.orgE<gt>, L<http://ali.as/>
 
 =head1 SEE ALSO
 
@@ -506,7 +506,7 @@ most of the functionality for this class.
 
 =head1 COPYRIGHT
 
-Copyright (c) 2002 - 2006 Adam Kennedy. All rights reserved.
+Copyright (c) 2002 - 2006 Adam Kennedy.
 
 This program is free software; you can redistribute
 it and/or modify it under the same terms as Perl itself.
